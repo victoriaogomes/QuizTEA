ebe793a64f602bfe5e635f59c2dbe839b7342413
Victória Gomes <38071157+victoriaogomes@users.noreply.github.com>
2019-04-14 22:55:09 -0300

    more editions

--- Game/screens/titleScreenManager.gd
+++ Game/screens/titleScreenManager.gd
@@ -5,10 +5,15 @@ func _ready():
 
 
 func _on_play_pressed():
-	$option1/play.set_pickable(false)
-	$instructions.set_pickable(false)
-	$option3/about.set_pickable(false)
+	print("cliquei")
+	$option1/play.set_shape_visible(false)
+	$instructions.set_shape_visible(false)
+	$option3/about.set_shape_visible(false)
+	get_node("/root/Node2D/AnimationPlayer").play("transition", -1, 1.0, false)
+	yield(get_node("/root/Node2D/AnimationPlayer"), "animation_finished")
 	$storySelectScene.set_visible(true)
+	get_node("/root/Node2D/storySelectScene/AnimationPlayer").play("transition2", -1, 1.0, false)
+	yield(get_node("/root/Node2D/storySelectScene/AnimationPlayer"), "animation_finished")
 
 
 func _on_instructions_pressed():
